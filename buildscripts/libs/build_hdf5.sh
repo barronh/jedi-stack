71b91616ff7d3a1672cc274e5b4e74682a0f2f75
Barron H. Henderson <barronh@gmail.com>
2024-02-08 16:14:26 -0500

    Update build_hdf5.sh
    HDF5 git repository is now on github.com. The bitbucket.hdfgroup.org version now requires a password. The easy answer is to switch to github.

--- buildscripts/libs/build_hdf5.sh
+++ buildscripts/libs/build_hdf5.sh
@@ -50,7 +50,7 @@ export FCFLAGS="$FFLAGS"
 SZIP_ROOT=${SZIP_ROOT:-/usr}
 ZLIB_ROOT=${ZLIB_ROOT:-/usr}
 
-gitURL="https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git"
+gitURL="https://github.com/HDFGroup/hdf5"
 
 cd ${JEDI_STACK_ROOT}/${PKGDIR:-"pkg"}
 
